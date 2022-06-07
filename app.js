b39e6ae15140e3518ebe02195ef17d9103645a48
Michelle Clement <michellelclement@gmail.com>
2022-06-07 10:25:16 +0000

    Notifications incon animation

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const tl = gsap.timeline({
   defaults: { duration: 0.35, ease: "Power2.easeOut" }
 });
 const home = document.querySelector(".home");
+const notifications = document.querySelector(".notifications");
+const messages = document.querySelector(".messages");
 
+//Home animation
 gsap.set(".feather", { scale: 0, transformOrigin: "center" });
 home.addEventListener("click", () => {
   gsap.fromTo(".home-svg", { scale: 1 }, { scale: 0.9, yoyo: true, repeat: 1 });
@@ -13,4 +16,26 @@ home.addEventListener("click", () => {
   );
   gsap.fromTo(".right-feather", { x: 0 }, { x: 5 });
   gsap.fromTo(".left-feather", { x: 0 }, { x: -2 });
+});
+
+//Notification Annimation
+gsap.set(".bell", { transformOrigin: "top center" });
+gsap.set(".ringer", { transformOrigin: "top center" });
+gsap.set(".wave", { opacity: 0, transformOrigin: "bottom" });
+notifications.addEventListener("click", () => {
+  gsap.fromTo(
+    ".bell",
+    { rotation: -5 },
+    { rotation: 0, duration: 2, ease: "elastic.out(5, 0.2)" }
+  );
+  gsap.fromTo(
+    ".ringer",
+    { rotation: -3, x: 0.5 },
+    { rotation: 0, x: 0, duration: 1, ease: "elastic.out(5, 0.2)" }
+  );
+  gsap.fromTo(
+    ".wave",
+    { scale: 0, opacity: 1 },
+    { scale: 1.3, opacity: 0, duration: 1 }
+  );
 });
