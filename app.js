005e24f687b31c7a7a94be0e20a61939fd1458ec
Michelle Clement <michellelclement@gmail.com>
2022-06-07 10:41:36 +0000

    Animate messages finished

--- app.js
+++ app.js
@@ -39,3 +39,17 @@ notifications.addEventListener("click", () => {
     { scale: 1.3, opacity: 0, duration: 1 }
   );
 });
+
+//Messages Animation
+gsap.set(".flap", { transformOrigin: "top" });
+messages.addEventListener("click", () => {
+  tl.fromTo(".messages-svg", { scale: 1 }, { scale: 0.9 });
+  tl.fromTo(".flap", { scale: 1 }, { scale: -1 }, "<50%");
+  tl.fromTo(".messages-svg", { scale: 0.9 }, { scale: 1 }, "<50%");
+  tl.fromTo(
+    ".note",
+    { y: 0, opacity: 1 },
+    { y: -40, opacity: 0, duration: 0.75 }
+  );
+  tl.to(".flap", { scale: 1 }, "<50%");
+});
